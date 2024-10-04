5fe6439c174b4866d086dcee522db51caf4fd6a0
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-10-04 23:20:10 +0000

    template literals

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+// ES6: Use Arrow Functions to Write Concise Anonymous Functions
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
