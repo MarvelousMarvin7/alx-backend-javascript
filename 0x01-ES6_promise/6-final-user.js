cd26dd4b187a2382df9b8f2740f9a05cdbdeb97f
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-10-10 15:03:17 +0000

    multiple user promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(filename);
+  const promises = [promise1, promise2];
+  return Promise.allSettled(promises).then((values) => {
+    const result = [];
+    values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+        result.push({ status: value.status, value: value.value });
+      } else {
+        result.push({ status: value.status, value: value.reason });
+      }
+    });
+    return result;
+  });
+}
