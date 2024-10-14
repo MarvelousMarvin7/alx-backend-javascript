21c00588239ce3dcd05dc80507c11a687884f1fe
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-10-14 19:12:13 +0000

    try fix

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        queue.push(mathFunction());   
-    } catch (error) {
-        queue.push(`Error: ${error.message}`);
-    } finally {
-        queue.push('Guardrail was processed');
-    }
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
 
-    return queue;
+  return queue;
 }
