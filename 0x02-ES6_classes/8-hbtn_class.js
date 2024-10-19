8168325cf417849abec7bbb059ee347109afdb7d
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-10-19 22:27:27 +0000

    js class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+// HolbertonClass object
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
