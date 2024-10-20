0e7773b1cdd9419a525094bbfa097dc8f76d9065
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-10-20 16:04:50 +0000

    js class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,7 @@
 import Car from './10-car';
 
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
